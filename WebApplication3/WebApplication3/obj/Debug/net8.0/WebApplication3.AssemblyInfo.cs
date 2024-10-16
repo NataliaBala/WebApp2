289a369d61644c684ca797a07c25eb1c045922e3
Natalia Bała <natalia.bala@microsoft.wsei.edu.pl>
2024-10-16 18:44:07 +0200

    4

--- WebApplication3/WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa224e826de2f44b67f148f98567bf7480489e42")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
